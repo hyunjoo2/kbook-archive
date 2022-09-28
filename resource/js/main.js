038a89a92bf1b6b072152dffcbda4ccd29375e5c
hyunjoo2 <ttldlguswn@gmail.com>
2022-09-28 12:21:13 +0900

    메인 반응형 작업중

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,10 +1,18 @@
 $(function(){
     searchBookDetail();
+    clickTab();
 });
 
 function searchBookDetail(){
     $(".search-book-detail__button").click(function(){
         $(this).toggleClass("is-open");
         $(".form-search-book__bottom").slideToggle();
+    })
+}
+
+function clickTab(){
+    $(".search-book--tab__item, .book-list__type-item").click(function(){
+        $(this).addClass("is-active");
+        $(this).siblings().removeClass("is-active");
     })
 }
